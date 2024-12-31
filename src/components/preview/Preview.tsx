c9b3f53e32602ed9fa70d43ba8a8e31f8a5b9f5a
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-12-31 20:00:49 +0530

    add: dark mode for syntax highlighting

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -5,101 +5,91 @@ import remarkGfm from "remark-gfm";
 import rehypeSanitize from "rehype-sanitize";
 import rehypeHighlight from "rehype-highlight";
 import rehypeRaw from "rehype-raw";
-import 'highlight.js/styles/github.css'; // Example theme
-
+import 'highlight.js/styles/github-dark.css';
 
 interface PreviewProps {
-	text: string;
+  text: string;
 }
 
 function adjustMdText(text: string) {
-	if (!text) return text;
-	text = text.replace(/<br \/>/g, "\n");
-	const lines = text.split("\n");
-	let insideCodeBlock = false;
+  if (!text) return text;
+  text = text.replace(/<br \/>/g, "\n");
+  const lines = text.split("\n");
+  let insideCodeBlock = false;
   
-	for (let i = 0; i < lines.length; i++) {
-	  const line = lines[i];
-	  if (line.trim().startsWith("```")) {
-		if (!insideCodeBlock && line.trim() === "```") {
-		  lines[i] = "```plaintext";
-		}
-		insideCodeBlock = !insideCodeBlock;
-	  }
-	}
-	
-	return lines.join("\n");
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.trim().startsWith("```")) {
+      if (!insideCodeBlock && line.trim() === "```") {
+        lines[i] = "```plaintext";
+      }
+      insideCodeBlock = !insideCodeBlock;
+    }
+  }
+  return lines.join("\n");
 }
 
 const Preview: React.FC<PreviewProps> = ({ text }) => {
-	return (
-		<div className="mt-5 mx-3 w-[50%] min-h-[70vh] max-h-[70vh] overflow-scroll p-6 text-black bg-white rounded-lg shadow-lg flex-1 sm:w-[90%]">
-			<ReactMarkdown
-				remarkPlugins={[remarkGfm]}
-				rehypePlugins={[rehypeSanitize, rehypeHighlight, rehypeRaw]} // Add plugins as necessary
-				components={{
-					// Inline Code
-					code({ inline, className, children, ...props }) {
-						const isInline = inline || !className;
-						if (isInline) {
-							return (
-								<span className="bg-gray-200 font-mono text-black px-1 rounded">
-									{children}
-								</span>
-							);
-						} else {
-							return (
-								<pre className="bg-gray-800 text-white p-4 rounded mt-2 overflow-auto">
-									<code {...props} className={className}>
-										{children}
-									</code>
-								</pre>
-							);
-						}
-					},
-					// links
-					a({ children, href }) {
-						return (
-							<a href={href} className="text-blue-500 hover:underline">
-								{children}
-							</a>
-						);
-					},
-					// Tables
-					table({ children }) {
-						return (
-							<table className="table-auto w-full border-collapse my-4">
-								{children}
-							</table>
-						);
-					},
-					// Table Rows
-					tr({ children }) {
-						return <tr className="border-b">{children}</tr>;
-					},
-					// Table Header
-					th({ children }) {
-						return (
-							<th className="text-left py-2 px-4 border-b bg-gray-200">
-								{children}
-							</th>
-						);
-					},
-					// Table Data Cells
-					td({ children }) {
-						return <td className="py-2 px-4 border-b">{children}</td>;
-					},
-					//line break
-					br() {
-						return <br />;
-					}
-
-				}}
-			>
-				{adjustMdText(text)}
-			</ReactMarkdown>
-		</div>
-	);
+  return (
+    <div className="mt-5 mx-3 w-[50%] min-h-[70vh] max-h-[70vh] overflow-scroll p-6 text-black bg-white rounded-lg shadow-lg flex-1 sm:w-[90%]">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeSanitize, rehypeHighlight, rehypeRaw]}
+        components={{
+          code({ inline, className, children, ...props }) {
+            const isInline = inline || !className;
+            if (isInline) {
+              return (
+                <span className="bg-gray-800 font-mono text-gray-200 px-1 rounded">
+                  {children}
+                </span>
+              );
+            } else {
+              return (
+                <pre className="bg-gray-900 text-gray-100 p-4 rounded mt-2 overflow-auto">
+                  <code {...props} className={className}>
+                    {children}
+                  </code>
+                </pre>
+              );
+            }
+          },
+          a({ children, href }) {
+            return (
+              <a href={href} className="text-blue-400 hover:underline">
+                {children}
+              </a>
+            );
+          },
+          table({ children }) {
+            return (
+              <table className="table-auto w-full border-collapse my-4">
+                {children}
+              </table>
+            );
+          },
+          tr({ children }) {
+            return <tr className="border-b">{children}</tr>;
+          },
+          th({ children }) {
+            return (
+              <th className="text-left py-2 px-4 border-b bg-gray-800 text-gray-100">
+                {children}
+              </th>
+            );
+          },
+          td({ children }) {
+            return <td className="py-2 px-4 border-b">{children}</td>;
+          },
+          br() {
+            return <br />;
+          }
+        }}
+      >
+        {adjustMdText(text)}
+      </ReactMarkdown>
+    </div>
+  );
 };
 
 export default Preview;
